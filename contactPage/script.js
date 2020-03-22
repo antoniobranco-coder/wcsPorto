991b9131952bb88d0c8c24de67863e196d7bbf92
Antonio Branco <antoniobranco@sapo.pt>
2020-03-22 11:57:44 +0000

    Version 2

---
+++ contactPage/script.js
@@ -0,0 +1,20 @@
+
+            function initMap()
+            {
+                var options=
+              {
+                zoom: 4,
+                center: {lat:42.36,lng:-71.06}
+              };
+                var map = new google.maps.Map(document.getElementById("map"), options);
+              var location = {lat: -25.363, lng: 131.044};
+              
+              var marker = new google.maps.Marker({
+                position: {lat:42.4777,lng:-70.9595}
+              })  
+              //AIzaSyCRb_3Q2QnuWzxG21HJJN9nDHSnHD9P2vk
+            };
+          
+          async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRb_3Q2QnuWzxG21HJJN9nDHSnHD9P2vk&callback=initMap">
+        
+          
